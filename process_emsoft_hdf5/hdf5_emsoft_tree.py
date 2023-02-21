7f15a5432fb8b2808371aa16b4911b6f0f7ae418
Lukas Berners <berners@imm.rwth-aachen.de>
2023-02-21 15:28:26 +0100

    added loops for headers monte carlo and master

--- process_emsoft_hdf5/hdf5_emsoft_tree.py
+++ process_emsoft_hdf5/hdf5_emsoft_tree.py
@@ -8,23 +8,53 @@ import h5py
 #import tkinter
 
 #matplotlib.use('Agg')
-#_tkinter.TclError: no display name and no $DISPLAY environment variable
+#_tkinter.TclEr>ror: no display name and no $DISPLAY environment variable
 from tkinter.filedialog import askopenfilename
 from pathlib import *
 #import re
 import cv2 as cv2
 import numpy as np
 import os.path as path
+import pandas as pd
 #%%
 file_name="Nb6_Co7_MC_20_kv_500px.h5"
 file_name=Path(file_name)
 f= h5py.File(file_name)
 #%%
 
+df=pd.DataFrame()
+#%%
 def print_attrs(name, obj):
-    print name
-    for key, val in obj.attrs.iteritems():
-        print "    %s: %s" % (key, val)
+    #df=pd.DataFrame()
+    print( name)
+    #i=0
+    namelist=[]
+    for key, val in obj.attrs.items():
+     #   i=i+1
+        print( "    %s: %s" % (key, val))
+     #   df["i"]=i
+        #keylist=keylist.append(key)
+    namelist=namelist.append(name)
+    return(name)
 
-f = h5py.File('foo.hdf5','r')
-f.visititems(print_attrs)
+keylist=f.visititems(print_attrs)
+#%%
+key
+# %%
+
+
+#def read_emmaster_meta(f)
+EMheader=f['EMheader']
+master_head=EMheader['EBSDmaster']
+mc_head=EMheader['MCOpenCL']
+
+#%%
+#master_head[keys]
+df=pd.DataFrame()
+for key in master_head.keys():
+    print(master_head[key])
+    df[(f"master_{key}")]=master_head[key]
+for key in mc_head.keys():
+    print(mc_head[key])
+    df[(f"mc_{key}")]=master_head[key]
+# %%
