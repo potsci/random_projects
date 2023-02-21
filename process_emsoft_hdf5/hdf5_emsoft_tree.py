df051381ae07d4cb37c22d7333cee4444d57bf71
Lukas Berners <berners@imm.rwth-aachen.de>
2023-02-21 16:30:48 +0100

    header and nml data of h5 files inside a certain folder are saved to pandas

--- process_emsoft_hdf5/hdf5_emsoft_tree.py
+++ process_emsoft_hdf5/hdf5_emsoft_tree.py
@@ -1,60 +1,62 @@
 import os
 import matplotlib as mpl
-if os.environ.get('DISPLAY','') == '':
+if os.environ.get('DISPLAY', '') == '':
     print('no display found. Using non-interactive Agg backend')
     mpl.use('Agg')
 import matplotlib.pyplot as pltd
 import h5py
-#import tkinter
+# import tkinter
 
-#matplotlib.use('Agg')
-#_tkinter.TclEr>ror: no display name and no $DISPLAY environment variable
+# matplotlib.use('Agg')
+# _tkinter.TclEr>ror: no display name and no $DISPLAY environment variable
 from tkinter.filedialog import askopenfilename
 from pathlib import *
-#import re
+# import re
 import cv2 as cv2
 import numpy as np
 import os.path as path
 import pandas as pd
-#%%
-file_name="Nb6_Co7_MC_20_kv_500px.h5"
-file_name=Path(file_name)
-f= h5py.File(file_name)
-#%%
-
-df=pd.DataFrame()
-#%%
-def print_attrs(name, obj):
-    #df=pd.DataFrame()
-    print( name)
-    #i=0
-    namelist=[]
-    for key, val in obj.attrs.items():
-     #   i=i+1
-        print( "    %s: %s" % (key, val))
-     #   df["i"]=i
-        #keylist=keylist.append(key)
-    namelist=namelist.append(name)
-    return(name)
-
-keylist=f.visititems(print_attrs)
-#%%
-key
 # %%
 
-
-#def read_emmaster_meta(f)
-EMheader=f['EMheader']
-master_head=EMheader['EBSDmaster']
-mc_head=EMheader['MCOpenCL']
-
-#%%
-#master_head[keys]
-df=pd.DataFrame()
-for key in master_head.keys():
-    print(master_head[key])
-    df[(f"master_{key}")]=master_head[key]
-for key in mc_head.keys():
-    print(mc_head[key])
-    df[(f"mc_{key}")]=master_head[key]
+# file_name="Nb6_Co7_MC_20_kv_500px.h5"
+# file_name=Path(file_name)
+# f= h5py.File(file_name)
 # %%
+dirname = os.path.dirname(__file__)
+file_name = 'hdf5/'
+file_name = os.path.join(dirname, file_name)
+file_name = Path(file_name)
+paths = list(file_name.glob('*'))
+dfs = pd.DataFrame()
+i = 0
+for path in paths:
+    print(i)
+    i = i+1
+    f = h5py.File(path)
+    df = pd.DataFrame()
+    
+    EMheader = f['EMheader']
+    if 'EBSDmaster' in EMheader.keys():
+        master_head = EMheader['EBSDmaster']
+        for key in master_head.keys():
+            df[(f"master_{key}")] = master_head[key]
+
+    if 'MCOpenCL' in EMheader.keys():
+        mc_head = EMheader['MCOpenCL']
+        for key in mc_head.keys():
+            df[(f"mc_{key}")] = mc_head[key]
+
+    NMLpars = f['NMLparameters']
+    if 'EBSDMasterNameList' in NMLpars.keys():
+        nml_master = NMLpars['EBSDMasterNameList']
+        for key in nml_master.keys():
+            df[(f"nml_master_{key}")] = nml_master[key]
+
+    if 'MCCLNameList' in NMLpars.keys():
+        nml_mc = NMLpars['MCCLNameList']
+        for key in nml_mc.keys():
+            df[(f"nml_mc_{key}")] = nml_mc[key]
+
+    dfs = pd.concat([dfs, df])
+# %%
+#pd.concat(df)
